291a6ca37f2c18b40895f712d2e878a789ffb9c6
LorisMarzocchi <ml77182a@gmail.com>
2023-06-27 17:20:34 +0200

    addMigrations

---
+++ database/migrations/2023_06_27_143950_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('company', 100);
+            $table->string('departure_station', 200);
+            $table->string('arrival_station', 200);
+            $table->date('departure_time');
+            $table->date('arrival_time');
+            $table->string('train_code', 50);
+            $table->smallInteger('carriages')->nullable();
+            $table->boolean('in_time');
+            $table->boolean('deleted')->default(false);
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
